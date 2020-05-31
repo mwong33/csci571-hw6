a5eff8110cc51836cea511944e709b03d116a467
Matthew James Wong <matthew.wong76@gmail.com>
2020-05-31 11:28:49 -0600

    Adding js for price range validation

---
+++ script.js
@@ -0,0 +1,16 @@
+// Form Validation
+const form = document.getElementById("form");
+const fromPrice = document.getElementById("from_price");
+const toPrice = document.getElementById("to_price");
+
+form.addEventListener('submit', (e) => {
+    
+    if (fromPrice.value > toPrice.value) {
+        e.preventDefault();
+        alert("Oops! Lower price limit cannot be greater than upper price limit! Please try again.");
+    } else if (fromPrice.value < 0 || toPrice.value < 0) {
+        e.preventDefault();
+        alert("Price Range values cannot be negative! Please try a value greater than or equal to 0.0");
+    }
+
+})
