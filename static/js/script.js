f7fd5159c4a2427ea91164e50352147e7f04c78e
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-04 12:13:08 -0600

    oh so this is how promises work

--- static/js/script.js
+++ static/js/script.js
@@ -15,94 +15,120 @@ form.addEventListener("submit", (e) => {
     } else if (priceFrom.value < 0 || priceTo.value < 0) {
         alert("Price Range values cannot be negative! Please try a value greater than or equal to 0.0");
     } else {
-        getItems();
+        const items = () => {
+            getItems().then(itemsJSON => {
+                console.log(itemsJSON);
+                displayResultCount(parseInt(itemsJSON["totalResults"]));
+            });
+        };
+
+        items();
         //displayResultCount(parseInt(itemsJSON["totalResults"]));
         //createItemCard();
     }
-})
+});
 
 // Function to execute GET request to our Python Flask server
 function getItems() {
 
-    // Get Form Data
-    let keywords = document.getElementById("key_words").value;
+    const promise = new Promise((resolve, reject) => {
+        // Get Form Data
+        let keywords = document.getElementById("key_words").value;
 
-    let request_url = "/items?keywords=" + keywords;
+        let request_url = "/items?keywords=" + keywords;
 
-    if (document.getElementById("price_from").value != "") {
-        request_url += "&price_from=" + document.getElementById("price_from").value;
-    }
-
-    if (document.getElementById("price_to").value != "") {
-        request_url += "&price_to=" + document.getElementById("price_to").value;
-    }
-
-    if (document.getElementById("new").checked == true) {
-        request_url += "&new=" + document.getElementById("new").value;
-    }
-
-    if (document.getElementById("used").checked == true) {
-        request_url += "&used=" + document.getElementById("used").value;
-    }
-
-    if (document.getElementById("very_good").checked == true) {
-        request_url += "&very_good=" + document.getElementById("very_good").value;
-    }
-
-    if (document.getElementById("good").checked == true) {
-        request_url += "&good=" + document.getElementById("good").value;
-    }
-
-    if (document.getElementById("acceptable").checked == true) {
-        request_url += "&acceptable=" + document.getElementById("acceptable").value;
-    }
-
-    if (document.getElementById("return_accepted").checked == true) {
-        request_url += "&return_accepted=" + document.getElementById("return_accepted").value;
-    }
-
-    if (document.getElementById("free_shipping").checked == true) {
-        request_url += "&free_shipping=" + document.getElementById("free_shipping").value;
-    }
-
-    if (document.getElementById("expedited_shipping").checked == true) {
-        request_url += "&expedited_shipping=" + document.getElementById("expedited_shipping").value;
-    }
-    
-    request_url += "&sort_by=" + document.getElementById("sort_by").value;
-    
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            const data = JSON.parse(xhttp.responseText);
-            console.log(data);
+        if (document.getElementById("price_from").value != "") {
+            request_url += "&price_from=" + document.getElementById("price_from").value;
         }
 
-        if (this.status == 404) {
-            console.log("File or resource not found!");
+        if (document.getElementById("price_to").value != "") {
+            request_url += "&price_to=" + document.getElementById("price_to").value;
         }
-    };
 
-    xhttp.open("GET", request_url, true);
-    xhttp.send();
-}
+        if (document.getElementById("new").checked == true) {
+            request_url += "&new=" + document.getElementById("new").value;
+        }
+
+        if (document.getElementById("used").checked == true) {
+            request_url += "&used=" + document.getElementById("used").value;
+        }
+
+        if (document.getElementById("very_good").checked == true) {
+            request_url += "&very_good=" + document.getElementById("very_good").value;
+        }
+
+        if (document.getElementById("good").checked == true) {
+            request_url += "&good=" + document.getElementById("good").value;
+        }
+
+        if (document.getElementById("acceptable").checked == true) {
+            request_url += "&acceptable=" + document.getElementById("acceptable").value;
+        }
+
+        if (document.getElementById("return_accepted").checked == true) {
+            request_url += "&return_accepted=" + document.getElementById("return_accepted").value;
+        }
+
+        if (document.getElementById("free_shipping").checked == true) {
+            request_url += "&free_shipping=" + document.getElementById("free_shipping").value;
+        }
+
+        if (document.getElementById("expedited_shipping").checked == true) {
+            request_url += "&expedited_shipping=" + document.getElementById("expedited_shipping").value;
+        }
+        
+        request_url += "&sort_by=" + document.getElementById("sort_by").value;
+        
+        let xhttp = new XMLHttpRequest();
+        xhttp.responseType = 'json';
+
+        xhttp.onload = () => {
+            if (xhttp.status == 200) {
+                resolve(xhttp.response);
+            }
+
+            if (xhttp.status >= 400) {
+                reject(xhttp.response);
+            }
+        };
+
+        xhttp.open("GET", request_url, true);
+        xhttp.send();
+    });
+
+    return promise;
+};
 
 // Function to display the result count
 function displayResultCount(count) {
+    // Remove the curren result count if present
+    if(document.getElementById("result_count") != null) {
+        let removeResultCountDiv = document.getElementById("result_count");
+        document.body.removeChild(removeResultCountDiv);
+    }
 
+    // Display the current result count
     if (count == 0) {
         let resultCountDiv = document.createElement("div");
-        resultCountDiv.setAttribute("class", "result_count");
+        resultCountDiv.setAttribute("id", "result_count");
 
         let resultCount = document.createElement("h2");
         resultCount.innerHTML = "No Results found"
-        resultDiv.appendChild(resultCount);
+        resultCountDiv.appendChild(resultCount);
 
-        document.body.appendChild(resultDiv);
+        document.body.appendChild(resultCountDiv);
+    } else {
+        let resultCountDiv = document.createElement("div");
+        resultCountDiv.setAttribute("id", "result_count");
+
+        let resultCount = document.createElement("h2");
+        resultCount.innerHTML = count + " Results found for " + document.getElementById("key_words").value;
+        resultCountDiv.appendChild(resultCount);
+        
+        document.body.appendChild(resultCountDiv);
     }
 
-}
+};
 
 // Function to create an item card
 function createItemCard() {
@@ -123,4 +149,4 @@ function createItemCard() {
     cardDiv.appendChild(price);
 
     document.body.appendChild(cardDiv);
-}
+};
